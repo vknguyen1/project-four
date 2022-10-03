2b134ede591d4cf82a0bfda881d151d57c3af1a5
hannahbethke <hannahnichole@gmail.com>
2022-10-02 22:04:33 -0600

    adds about url path to code pulled from IP

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('search', views.search, name='search')
+    path('search', views.search, name='search'),
+    path('', views.about, name='about'),
 ]
