9f42ea72f5608ac8eb90150c6b75e8b28e901da2
hannahbethke <hannahnichole@gmail.com>
2022-10-04 22:18:59 -0600

    adds UserProfile model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse # reverse lookup- URL path builder
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
