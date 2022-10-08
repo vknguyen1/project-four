36243a2f1a4a0844e2e02e4ebc8731ec32f01f62
irwinpanguripan <irwinpanguripan@gmail.com>
2022-10-08 10:44:21 -0700

    added artist_detail page with routing

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('about/', views.about, name='about'),
     path('events/detail/', views.detail, name='detail'),
+    path('artists/detail/', views.artist_detail, name='artist_detail'),
     path('user_profile/', views.user_profile, name='user_profile'),
     path('accounts/signup/', views.signup, name='signup')
 ]
