c6985d1caa42a9a254c5b4b368631ba3b9e05427
irwinpanguripan <irwinpanguripan@gmail.com>
2022-10-08 10:48:43 -0700

    added artist detail page and artist api call

--- main_app/views.py
+++ main_app/views.py
@@ -17,6 +17,8 @@ BASE_URL = 'https://api.seatgeek.com/2/'
 
 event = 'events'
 
+performers = 'performers/'
+
 postal_code = 'postal_code='
 
 query = '?'
@@ -25,13 +27,30 @@ clientID_secret = env('clientID_secret')
 
 
 def call_api_with_filters_for_event(parameters):
-    filter_event_URL = BASE_URL + event + query + "type=concert&"
+    filter_concert_URL = BASE_URL + event + query + "type=concert&"
+    filter_festival_URL = BASE_URL + event + query + "type=music_festival&"
     for key in parameters:
-        filter_event_URL = filter_event_URL + key + '=' + parameters[key] + '&'
-    filter_event_URL = filter_event_URL + clientID_secret
-    response = requests.get(filter_event_URL)
-    json = response.json()
-    return json
+        filter_concert_URL = filter_concert_URL + key + '=' + parameters[key] + '&'
+    filter_concert_URL = filter_concert_URL + clientID_secret
+    for key in parameters:
+        filter_festival_URL = filter_festival_URL + key + '=' + parameters[key] + '&'
+    filter_festival_URL = filter_festival_URL + clientID_secret
+    concert_response = requests.get(filter_concert_URL)
+    festival_response = requests.get(filter_festival_URL)
+    concert_json = concert_response.json()
+    festival_json = festival_response.json()
+    return (concert_json, festival_json)
+
+
+def call_api_for_artist_data(parameters):
+    filter_artist_URL = BASE_URL + performers 
+    for key in parameters:
+        filter_artist_URL = filter_artist_URL + parameters[key] + '?' + clientID_secret
+    print (filter_artist_URL)
+    artist_response = requests.get(filter_artist_URL)
+    
+    artist_json = artist_response.json()
+    return artist_json
 
 
 def home(request):
@@ -48,8 +67,8 @@ def search(request):
         elif key == "performers.slug":
             queries[key] = queries[key].replace(" ", "-").lower()
 
-    events = call_api_with_filters_for_event(queries)        
-    return render(request, 'events/search.html', {'events':events, 'page_name': 'Events'})
+    events = call_api_with_filters_for_event(queries)     
+    return render(request, 'events/search.html', {'events':events[0], 'festivals':events[1], 'page_name': 'Events'})
 
 
 def about(request):
@@ -58,6 +77,11 @@ def about(request):
 
 def detail(request):
     return render(request, 'events/detail.html', {'page_name': 'Detail'})
+
+def artist_detail(request):
+    queries = request.GET.copy()
+    artist = call_api_for_artist_data(queries)
+    return render(request, 'artists/artist_detail.html', {'artist':artist})
 
 
 def signup(request):
